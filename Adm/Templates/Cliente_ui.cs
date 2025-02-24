1a26d38f10d5b4f035c239bf52f66db1c7401ec9
isacamilli <165300352+isacamilli@users.noreply.github.com>
2025-02-24 14:15:51 +0000

    termino cliente e começo armario

--- Adm/Templates/Cliente_ui.cs
+++ Adm/Templates/Cliente_ui.cs
@@ -42,16 +42,34 @@ namespace Adm.Templates
                             break;
 
                         case 2:
-                            
-                            int id_escolhido = listarId();
 
-                            Console.WriteLine("\nDeseja ver as roupas do cliente?");
-                            string escolha = Console.ReadLine(); 
-                            if (escolha.ToLower() == "sim"){
-                                
+                            Console.Write("\nDigite o nome do usuário que deseja escolher: ");
+
+                            string nome = Console.ReadLine();
+                            
+                            int id_escolhido = listarId(nome);
+
+                            if (id_escolhido != 0){
+                                Console.Write("\nDeseja ver as roupas do cliente?");
+                                string escolha = Console.ReadLine(); 
+                                if (escolha.ToLower() == "sim"){
+                                    Roupa_UI.lista_roupas_id(id_escolhido);
+                                }
                             }
 
                             break;
+
+                        case 3:
+                            excluir_cliente();
+
+                            break;
+
+                        case 4:
+                            numero = Roupa_UI.menu_armario();
+                            break;
+
+                        case 6:
+                            return 1;
                                         
                         default:
                             Console.WriteLine("\nEntrada inválida! Digite um número entre as opções.");
@@ -64,7 +82,7 @@ namespace Adm.Templates
                     numero = 1;
                 }
             }
-            return 1;
+            return 0;
         }
 
         public static void listar_clientes(){
@@ -73,12 +91,9 @@ namespace Adm.Templates
             }
         }
 
-        public static int listarId(){
-            Console.Write("Digite o nome do usuário que deseja escolher: ");
+        public static int listarId(string nome){
 
-            string nome = Console.ReadLine();
-
-            foreach(Cliente i in listar_clientes()){
+            foreach(Cliente i in Cliente_view.listar_clientes()){
                 if (nome == i.getNome() ){
                     Console.WriteLine($"\n{i}");
 
@@ -90,5 +105,16 @@ namespace Adm.Templates
             return 0;
 
         }
+
+        public static void excluir_cliente(){
+            Console.Write("\nDigite o nome do usuário que deseja excluir: ");
+            string nome = Console.ReadLine();
+
+            int id_escolhido = listarId(nome);
+
+            Cliente_view.excluir_cliente(id_escolhido);            
+        }
+
+
     }
 }
--- Adm/Templates/Roupa_ui.cs
+++ Adm/Templates/Roupa_ui.cs
@@ -3,17 +3,80 @@ using Adm.Models;
 
 namespace Adm.Templates{
     public class Roupa_UI {
-        public static void lista_roupas_id(string nome){
-            int id = 0;
 
-            foreach (Cliente i in Cliente_view.listar_clientes()){
-                if (i.getNome() == nome) {
-                    id = i.getId();
+        public static int menu_armario(){
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("\nTela de armario");
+
+            
+
+            int numero = 1;
+
+            while (numero != 0){
+                Console.WriteLine("\n-------------------------------------");
+
+                Console.WriteLine("\nEscolha uma das opções");
+
+                Console.WriteLine("\n1 - Listar roupas");
+                Console.WriteLine("2 - Filtrar roupa por usuário");
+                Console.WriteLine("3 - Filtrar roupa por tipo");
+                Console.WriteLine("4 - Voltar");
+                Console.WriteLine("0 - Sair do sistema");
+
+
+                Console.Write("\nDigite aqui: ");
+
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    switch (numero)
+                    { 
+                        case 0 :
+                            return 0;
+                            break;
+
+                        case 1 :
+                            Console.WriteLine("\n");
+                            break;
+
+                        case 4:
+                            return 1;
+                                        
+                        default:
+                            Console.WriteLine("\nEntrada inválida! Digite um número entre as opções.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nEntrada inválida! Digite um número.");
+                    numero = 1;
                 }
             }
+            
+            return 0;
+            
+        }
 
+        public static void listar_roupas(){
+            foreach (Roupa i in Roupa_view.listar_roupas()){
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void lista_roupas_id(int id){
+
+            int num = 0;
+            
             foreach(Roupa x in Roupa_view.listar_roupa_id(id)){
                 Console.WriteLine(x);
+                num++;
+            }
+
+            if (num>0){
+                Console.WriteLine($"\nNúmero de roupas encontradas: {num}");
+            }
+            else{
+                Console.WriteLine("Armário vazio");
             }
         }
 
